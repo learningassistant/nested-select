22eccfa7cd24574a257ae627d9abb0954bbee581
jlstamp-cg <jamie.stamp@cityandguilds.com>
2019-10-21 15:43:56 +0100

    Test with docs folder in project root

---
+++ docs/precache-manifest.97ab0f818aff057e5eebe3fb21873494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ea3cec512fdb07db54dbaaa5fd5913",
+    "url": "/nested-select/index.html"
+  },
+  {
+    "revision": "d192ff41a173019b26c6",
+    "url": "/nested-select/static/css/main.e10af059.chunk.css"
+  },
+  {
+    "revision": "fd674c27e02d8d76be65",
+    "url": "/nested-select/static/js/2.be40a915.chunk.js"
+  },
+  {
+    "revision": "d192ff41a173019b26c6",
+    "url": "/nested-select/static/js/main.09af4c9a.chunk.js"
+  },
+  {
+    "revision": "7835949d684f21c0257a",
+    "url": "/nested-select/static/js/runtime-main.279794e7.js"
+  }
+]);
